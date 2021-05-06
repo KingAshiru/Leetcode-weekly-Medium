3a7fd5114cf23dec1020c91b587b36379e7cde5a
Tobi Ashiru <tobiashiru58@gmail.com>
2021-05-06 21:36:39 +0100

    Create Construct Binary Tree From Preorder and Inorder Array.py

---
+++ Construct Binary Tree From Preorder and Inorder Array.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        idx = {}
+        
+        for i, node in enumerate(inorder):
+            idx[node] = i
+        
+        stack = []
+        head = None
+        
+        for val in preorder:
+            if not head:
+                head = TreeNode(val)
+                stack.append(head)
+            else:
+                node = TreeNode(val)
+                if idx[val] < idx[stack[-1].val]:
+                    stack[-1].left = node
+                else:
+                    while stack and idx[val] > idx[stack[-1].val]:
+                        u = stack.pop()
+                    u.right = node
+                stack.append(node)
+        return head
+                    

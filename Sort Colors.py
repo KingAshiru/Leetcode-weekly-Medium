1ba5d4b75a6b0f2543aa815ff4a60b55dd09f491
Tobi Ashiru <tobiashiru58@gmail.com>
2021-05-08 00:50:05 +0100

    Create Sort Colors.py

---
+++ Sort Colors.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        red = 0
+        blue = len(nums) - 1
+        white = 0
+        while white <= blue:
+            if nums[white] < 1:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[blue], nums[white] = nums[white], nums[blue]
+                blue -= 1

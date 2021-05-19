57b7e91edaeab3e0f6f18eeae9209eb2f4c83bc7
Tobi Ashiru <tobiashiru58@gmail.com>
2021-05-19 09:04:58 +0100

    Create Product of array except self.py

---
+++ Product of array except self.py
@@ -0,0 +1,9 @@
+res = [1]*len(nums)
+        lprod = 1
+        rprod = 1
+        for i in range(len(nums)):
+            res[i] *= lprod
+            lprod *= nums[i]
+            res[~i] *= rprod
+            rprod *= nums[~i]
+        return res

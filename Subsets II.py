77431433bf6fce6c66265cbe25070bc80775756b
Tobi Ashiru <tobiashiru58@gmail.com>
2021-06-10 09:24:14 +0100

    Create Subsets II.py

---
+++ Subsets II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return [[]]
+        res = [[]]
+        dic = collections.Counter(nums)
+        for key, val in dic.items():
+            tmp = []
+            for lst in res:
+                for i in range(1, val+1):
+                    tmp.append(lst+[key]*i)
+            res += tmp
+        return res
